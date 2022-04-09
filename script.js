0a95da6ce3f65798e57034c345498e829acfb0e2
Lud <instimila@gmail.com>
2022-04-09 02:11:20 +0200

    Primeira versao

---
+++ script.js
@@ -0,0 +1,14 @@
+/*Escolher quantas cartas o jogo tem que ter 
+        Soltar um prompt na tela pedindo ao usuario um numero que defina a quantidade de cartas :
+                Se o numero for par , iniciar o jogo ; 
+                Se o numero for impar , soltar alert informando que o jogo so pode ser iniciado com quantidade par de cartas
+        Pegar esse numero e criar as divs que o numero diga
+ function nivel()
+ variavel global: let numCartas      
+        
+
+
+
+//Variaveis globais
+let cartas
+*/
