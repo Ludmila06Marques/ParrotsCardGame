2f00756fea7ced55f886732437c27238e8360cf2
Lud <instimila@gmail.com>
2022-04-11 19:36:44 +0200

    versao 1

--- script.js
+++ script.js
@@ -1,14 +1,98 @@
-/*Escolher quantas cartas o jogo tem que ter 
-        Soltar um prompt na tela pedindo ao usuario um numero que defina a quantidade de cartas :
-                Se o numero for par , iniciar o jogo ; 
-                Se o numero for impar , soltar alert informando que o jogo so pode ser iniciado com quantidade par de cartas
-        Pegar esse numero e criar as divs que o numero diga
- function nivel()
- variavel global: let numCartas      
+/*
+ function prepararInicio(){
+       
+        Soltar um prompt na tela pedindo ao usuario um numero que defina a quantidade de cartas [ armazenar em qtdCartas] :
+                Se o numero for menor que 4 , maior que 14 e impar(divisao por 2 com resto diferente de 0)--> Numero invalido
+                Se numero for valido pegar o numero e criar as cartas
         
+}
+function gerarCartas(){
+        Saber qual numero o usuario digitou 
+        Acessar a classe jogo e colocar dentro as divs correspondentes a quantidade de cartas escolhidas pelo usuario
+                Para isso criar um contador que percorra todo o array de quantidade de 
+                
+}
+function selecionarCarta(){
+        Carta selecionada deve virar e mostrar a parte de atras , deve manter virada ate outra carta ser selecionada .
+        Caso sejam iguais , manter as 2 cartas viradas
+        Caso sejam diferentes , aguardar 1 segundo e voltar a virar as 2 cartas
 
 
+}
 
-//Variaveis globais
-let cartas
+
+function prepararInicio()
+function carregarImagens()
+function selecionarCarta()
+function deselecionar()
 */
+let imagens=[];
+let selecionada=[];
+gerarCartas();
+
+function carregarImagens(){
+        imagens=['<img src="/img/bobrossparrot.gif" >',
+        '<img src="/img/explodyparrot.gif" >',
+        '<img src="/img/fiestaparrot.gif" >',
+        '<img src="/img/metalparrot.gif" >',
+        '<img src="/img/revertitparrot.gif" >',
+        '<img src="/img/tripletsparrot.gif" >',
+        '<img src="/img/unicornparrot.gif" >']
+
+}
+function gerarCartas(){
+        carregarImagens();
+        selecionada=[]
+        let jogo = document.getElementById("jogo")
+        let cartas= []
+        for (let i = 0; i < 14 ;i++) {
+               cartas.push(`
+                <div class="area-carta" onclick="selecionarCarta(${i})">
+                <div class="carta" id="carta${i}">
+                <div class="cara traseira" id="traseira${i}">  ${imagens[0]}</div>
+                <div class="cara superior"> <img src="/img/front.png" > </div>
+                </div>
+                </div>
+                  `  )
+                  if(i % 2 == 1){
+                        imagens.splice(0,1);
+                }
+                
+        }
+       cartas.sort(() => Math.random() - 0.5)
+       jogo.innerHTML= cartas.join(" ")
+
+}
+
+
+function selecionarCarta(i){
+        let carta= document.getElementById("carta"+ i)
+        if(carta.style.transform != "rotateY(180deg)")
+        {
+                carta.style.transform = "rotateY(180deg)"
+                selecionada.push(i)
+        }
+        if(selecionada.length == 2){
+                deselecionar(selecionada);
+                selecionada=[];
+             
+        }
+}
+
+
+function deselecionar(selecionada) {
+        setTimeout(() => {
+            
+            let traseira1 = document.getElementById("traseira" + selecionada[0])
+            let traseira2 = document.getElementById("traseira" + selecionada[1])
+            if (traseira1.innerHTML != traseira2.innerHTML) {
+                let carta1 = document.getElementById("carta" + selecionada[0])
+                let carta2 = document.getElementById("carta" + selecionada[1])
+                carta1.style.transform = "rotateY(0 deg)"
+                carta2.style.transform = "rotateY(0 deg)"
+            }else{
+                traseira1.style.background = "plum"
+                traseira2.style.background = "plum"
+            }
+        }, 1000);
+    }
